fbfb91cd84f077d2cc4d6370da33370111e09a12
Mauricio Gomes <mauricio@edge14.com>
2010-02-08 15:33:52 -0500

    Added support for installing ruby-audio in OS X

--- ext/sndfile/extconf.rb
+++ ext/sndfile/extconf.rb
@@ -1,15 +1,15 @@
 require 'mkmf'
-$CFLAGS = '-I/opt/local/include -I/opt/local/lib/ruby/gems/1.8/gems/narray-0.5.9.7'
+$CFLAGS = '-I/opt/local/include -I/opt/local/lib/ruby/gems/1.8/gems/narray-0.5.9.7 -I/Library/Ruby/Gems/1.8/gems/narray-0.5.9.7'
 $LDFLAGS = '-L/opt/local/lib -L/usr/local/lib'
 system 'swig -ruby -I/usr/include -I/usr/local/include -I/opt/local/include sndfile.i'
 unless find_library 'sndfile', 'sf_open'
-  error 'You need to install libsndfile'
+  raise 'You need to install libsndfile'
   exit
 end
 dir_config 'narray', CONFIG['sitearchdir'], CONFIG['sitearchdir']
 # TODO /opt/local/lib/ruby/gems/1.8/gems/narray-0.5.9.4/narray.h
 unless have_header 'narray.h'
-  error 'You need to install NArray'
+  raise 'You need to install NArray =(0.5.9.7)'
   exit
 end
 create_makefile 'sndfile'
