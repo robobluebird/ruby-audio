dd46270ac9fb6142ff753031d529f8f6639fb909
Stephen Augenstein <perl.programmer@gmail.com>
2010-02-15 13:28:00 -0500

    Made some changes to extconf.rb that should allow it to adjust better to different build environments

--- ext/sndfile/extconf.rb
+++ ext/sndfile/extconf.rb
@@ -1,15 +1,31 @@
+require 'rubygems'
 require 'mkmf'
-$CFLAGS = '-I/opt/local/include -I/opt/local/lib/ruby/gems/1.8/gems/narray-0.5.9.7 -I/Library/Ruby/Gems/1.8/gems/narray-0.5.9.7'
+
+$CFLAGS = '-I/opt/local/include'
 $LDFLAGS = '-L/opt/local/lib -L/usr/local/lib'
-system 'swig -ruby -I/usr/include -I/usr/local/include -I/opt/local/include sndfile.i'
+
+# Add narray header to path
+matches = Gem.source_index.find_name 'narray', Gem::Requirement.default
+if matches.empty?
+  raise 'You need to install NArray gem'
+  exit
+end
+spec = matches.last
+$CFLAGS += " -I#{spec.full_gem_path}"
+
+# libsndfile requirements
 unless find_library 'sndfile', 'sf_open'
-  raise 'You need to install libsndfile'
+  raise 'You need to install libsndfile (http://www.mega-nerd.com/libsndfile/)'
   exit
 end
-dir_config 'narray', CONFIG['sitearchdir'], CONFIG['sitearchdir']
-# TODO /opt/local/lib/ruby/gems/1.8/gems/narray-0.5.9.4/narray.h
+
+# NArray gem requirements (double check that header there)
 unless have_header 'narray.h'
-  raise 'You need to install NArray =(0.5.9.7)'
+  raise 'You need to install NArray gem'
   exit
 end
+
+# Swig
+system 'swig -ruby -I/usr/include -I/usr/local/include -I/opt/local/include sndfile.i'
+
 create_makefile 'sndfile'
